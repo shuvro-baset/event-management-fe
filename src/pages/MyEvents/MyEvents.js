4881ad34a57a5896ed8adc57748ab0ed1ecf4ec1
shuvro baset <md.abdul.baset75@gmail.com>
2021-10-27 22:02:49 +0600

    my event done

--- src/pages/MyEvents/MyEvents.js
+++ src/pages/MyEvents/MyEvents.js
@@ -7,18 +7,34 @@ const MyEvents = () => {
         <>
             <NavBar></NavBar>
             <Container>
-                <Row>
+                <Row className="my-4">
                     <Col md={6}>
-                        <div className="d-flex">
+                        <div className="d-flex activities-item bg-white shadow rounded p-2">
                             <div className="event-img p-2">
-                                <img className="img-fluid" src="https://i.ibb.co/L6X60hy/animal-Shelter.png" alt="" />
+                                <img className="img-fluid rounded" src="https://i.ibb.co/L6X60hy/animal-Shelter.png" alt="" />
                             </div>
-                            <div className="event-text p-2">
+                            <div className="event-text p-2 d-flex flex-column justify-content-between w-75">
                                 <div>
                                     <h4>Event Title</h4>
                                     <h6>Event Date</h6>
                                 </div>
+                                <div className="d-flex justify-content-end">
+                                    <button className="btn btn-danger">Cancel</button>
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col md={6}>
+                        <div className="d-flex activities-item bg-light shadow rounded p-2">
+                            <div className="event-img p-2">
+                                <img className="img-fluid rounded" src="https://i.ibb.co/L6X60hy/animal-Shelter.png" alt="" />
+                            </div>
+                            <div className="event-text p-2 d-flex flex-column justify-content-between w-75">
                                 <div>
+                                    <h4>Event Title</h4>
+                                    <h6>Event Date</h6>
+                                </div>
+                                <div className="d-flex justify-content-end">
                                     <button className="btn btn-danger">Cancel</button>
                                 </div>
                             </div>
